5717b78701cffe20e7f2cff08737d42d87dc2989
Jarek0 <jaroslaw.bielec.1994@gmail.com>
2020-10-16 21:10:55 +0200

    dwadwa

--- src/main.kt
+++ src/main.kt
@@ -2,11 +2,16 @@ import QuadraticEquationCreator.create
 
 fun main() {
     while (true) {
-        println("Program rozwiazuje rownanie kwadratowe postaci:")
-        println("f(x) = a*x^2 + b*x + c")
-        println("Rozwiazanie szukane jest w zbiorze liczb rzeczywistych")
-        val equation: QuadraticEquation = create()
-        println("Równanie ma postać: $equation")
-        equation.solve()
+        try {
+            println("Program rozwiazuje rownanie kwadratowe postaci:")
+            println("f(x) = a*x^2 + b*x + c")
+            println("Rozwiazanie szukane jest w zbiorze liczb rzeczywistych")
+            val equation: QuadraticEquation = create()
+            println("Równanie ma postać: $equation")
+            equation.solve()
+        }
+        catch (e: Exception) {
+            println(e.message)
+        }
     }
 }

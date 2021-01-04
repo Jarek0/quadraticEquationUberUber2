2e33b2224fac927c00af6f3fa8a5ac72106c7a77
Jarek0 <jaroslaw.bielec.1994@gmail.com>
2021-01-04 19:33:39 +0100

    dwadwa

--- src/QuadraticEquation.kt
+++ src/QuadraticEquation.kt
@@ -2,10 +2,23 @@ import QuadraticEquationSolverFactory.create
 import QuadraticEquationFirstFactorValidator.isValid
 import java.math.BigDecimal
 
-data class QuadraticEquation(private val a: BigDecimal, private val b: BigDecimal, private val c: BigDecimal) {
+class QuadraticEquation {
 
-    init {
+    private var a: BigDecimal
+    private var b: BigDecimal
+    private var c: BigDecimal
+
+    constructor(a: BigDecimal, b: BigDecimal, c: BigDecimal) {
         if(!isValid(a)) throw InvalidQuadraticEquationFirstFactorValueException()
+        this.a = a
+        this.b = b
+        this.c = c
+    }
+
+    private constructor() {
+        this.a = BigDecimal.ZERO
+        this.b = BigDecimal.ZERO
+        this.c = BigDecimal.ZERO
     }
 
     fun solve() {
@@ -15,4 +28,15 @@ data class QuadraticEquation(private val a: BigDecimal, private val b: BigDecima
     }
 
     override fun toString() = "$a*x^2 + $b*x + $c = 0"
+
+    companion object {
+        fun create(a: BigDecimal, b: BigDecimal, c: BigDecimal): QuadraticEquation {
+            if(!isValid(a)) throw InvalidQuadraticEquationFirstFactorValueException()
+            val equation = QuadraticEquation()
+            equation.a = a
+            equation.b = b
+            equation.c = c
+            return equation
+        }
+    }
 }
